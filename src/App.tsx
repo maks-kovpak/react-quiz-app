edcb88ef8afa15e1b57e2dd450096170184aed90
Maksym Kovpak <kovpak.maksym@datawiz.io>
2023-12-05 17:46:00 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Typography } from 'antd';
+
+function App() {
+  return (
+    <Typography>
+      <Typography.Title>Hello world!</Typography.Title>
+    </Typography>
+  );
+}
+
+export default App;
